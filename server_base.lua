0b010f2883a2d724f7a04c5947d13cd1482dd603
stacking <toth.gergocke@gmail.com>
2018-01-16 22:49:41 +0100

    Kezdeti állás - szervezetlen

---
+++ server_base.lua
@@ -0,0 +1,33 @@
+-- Example:
+-- ["user"] = {["version"] = ..., ...}
+
+-- key with value nil ?
+-- reserved keys?
+-- only-readable?
+-- writeable?
+local service_hndl = {
+	["using.user"] = "", ["user"] = {};
+}
+-- -> use the built-in ACL manager!!
+
+--
+local hold_protos = {
+	
+}
+
+-- Checks if resources are loaded and run
+-- Set strict resource-handling if needed (config)
+function resource_subscribing()
+	
+end
+
+-- Create requested keys if can
+-- 
+function resource_setProto()
+	
+end
+
+function resource_setProtoFunction(method_type, method)
+	
+end
+
