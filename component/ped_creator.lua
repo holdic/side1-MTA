19418e3ea402fe37fcc58b875356506501b65c21
holdic <24752302+holdic@users.noreply.github.com>
2018-01-17 11:33:08 +0100

    Create ped_creator.lua

---
+++ component/ped_creator.lua
@@ -0,0 +1,10 @@
+
+function generatePed(ar_settings, optfn_pedinitiator)
+
+assert( type(ar_settings) == "table", langget_error("LC_coding", "arg_not-table"))
+assert( not optfn_initiator OR type(optfn_initiator) == "function", "")
+
+for
+local newPed = createPed(...)
+
+end
